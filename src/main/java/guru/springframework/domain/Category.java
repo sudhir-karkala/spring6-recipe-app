38101913a3927d5cf7dd24c8d8a15aa77f5a1ef5
sudhir-k <4837546+sudhir-karkala@users.noreply.github.com>
2024-07-13 18:39:20 +0530

    spring data jpa query methods

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -12,4 +12,28 @@ public class Category {
     private String description;
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 }
