edc55c6d2cc41f0dca47d19a0af68434382b1cf6
sudhir-k <4837546+sudhir-karkala@users.noreply.github.com>
2024-08-06 18:53:08 +0530

    add unit test for category model object

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+    private Category category;
+
+    @BeforeEach
+    private void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+        String description = "some description";
+        category.setDescription(description);
+        assertEquals(description, category.getDescription());
+    }
+}
