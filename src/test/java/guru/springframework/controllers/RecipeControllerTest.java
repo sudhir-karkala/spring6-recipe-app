7e57753041d42df34201927a16c19623e284786f
sudhir-k <4837546+sudhir-karkala@users.noreply.github.com>
2024-08-17 23:59:32 +0530

    add a view for recipe/show/{id} and refactor service and controller

---
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -0,0 +1,45 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class RecipeControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    RecipeController recipeController;
+
+    AutoCloseable autoCloseable;
+
+    @BeforeEach
+    public void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        recipeController = new RecipeController(recipeService);
+    }
+
+    @Test
+    void testGetRecipe() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+        when(recipeService.findById(1L)).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
+
+    }
+}
